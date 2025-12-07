1eb56086712232d2d525b9237a1efe16a62b7f2b
Jakub Serok <jakub.serok@wp.pl>
2025-12-07 12:39:02 +0100

    File explorer with template

---
+++ lib/template.ts
@@ -0,0 +1,8 @@
+export const templatePaths = {
+  REACT: "/vsclone-starters/react-ts",
+  NEXTJS: "/vsclone-starters/nextjs-new",
+  EXPRESS: "/vsclone-starters/express-simple",
+  VUE: "/vsclone-starters/vue",
+  HONO: "/vsclone-starters/hono-nodejs-starter",
+  ANGULAR: "/vsclone-starters/angular",
+};

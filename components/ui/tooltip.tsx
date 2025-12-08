2b8614c3470b37f6948ccf13a1ad01186df599a9
Jakub Serok <jakub.serok@wp.pl>
2025-12-08 19:16:44 +0100

    bug Fix

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -31,7 +31,13 @@ function Tooltip({
 function TooltipTrigger({
   ...props
 }: React.ComponentProps<typeof TooltipPrimitive.Trigger>) {
-  return <TooltipPrimitive.Trigger data-slot="tooltip-trigger" {...props} />
+  return (
+    <TooltipPrimitive.Trigger
+      data-slot="tooltip-trigger"
+      asChild
+      {...props}
+    />
+  )
 }
 
 function TooltipContent({
